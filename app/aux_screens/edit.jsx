3847d7115a6c5c38daa477fc1dd9b67b0b9ec5f3
Sgityne <Giovansaraiva@gmail.com>
2024-10-10 02:34:22 -0300

    added the edit option for avatar and banner

--- app/aux_screens/edit.jsx
+++ app/aux_screens/edit.jsx
@@ -1,25 +1,56 @@
 import { View, Text, TouchableOpacity, Image, ImageBackground, TextInput, Alert } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { router } from 'expo-router';
+import * as ImagePicker from "expo-image-picker";
+
 import { icons, images } from '../../constants';
 import { useGlobalContext } from '../../context/GlobalProvider'
-import { SafeAreaView } from 'react-native-safe-area-context';
 import FavoriteActivity from '../../components/FavoriteActivity';
-import { router } from 'expo-router';
-import { updateProfile } from '../../lib/appwrite';
+import { getCurrentUser, updateProfile } from '../../lib/appwrite';
 
 const edit = () => {
-  const { user } = useGlobalContext();
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const [uploading, setUploading] = useState(false)
   const [form, setForm] = useState({
     name: null,
     bio: null,
+    avatar: null,
+    banner: null,
   })
+
+  const openPicker = async (selectType) => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if(!result.canceled) {
+      if(selectType === 'banner') {
+        setForm({ ...form, banner: result.assets[0] })
+      }
+
+      if(selectType === 'avatar') {
+        setForm({ ...form, avatar: result.assets[0] })
+      }
+    }
+  }
+  
+
   const submit = async () => {
 
     setUploading(true);
 
     try {
       await updateProfile(form, user.$id);
+
+      setUser(null)
+      setIsLoggedIn(false)
+      const result = await getCurrentUser();
+      setUser(result);
+      setIsLoggedIn(true);
 
       Alert.alert("Sucess", "Profile updated successfully")
       router.push('/profile')
@@ -29,6 +60,8 @@ const edit = () => {
       setForm({
         name: null,
         bio: null,
+        avatar: null,
+        banner: null,
       })
 
       setUploading(false);
@@ -37,10 +70,10 @@ const edit = () => {
 
   return (
     <SafeAreaView className="bg-primary h-full">
-      <View className="w-full h-[316px] justify-between">
+      <View className=" h-[316px] justify-between">
 
         <ImageBackground
-          source={{uri: user?.banner}}
+          source={{uri: form.banner?.uri ?? user?.banner}}
           className="absolute w-full h-60 self-start"
           resizeMode='cover'
         />
@@ -68,8 +101,11 @@ const edit = () => {
           </TouchableOpacity>
         </View>
 
-        <TouchableOpacity className="w-full pt-[38px] items-center">
-
+        <TouchableOpacity
+          onPress={() => openPicker('banner')}
+          className="w-full pt-[38px] items-center"
+        >
+  
           <Image
             source={images.changePhoto}
             className={"w-12 h-12"}
@@ -77,9 +113,9 @@ const edit = () => {
           />
         </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => openPicker('avatar')}>
           <Image
-            source={{uri: user?.avatar}}
+            source={{uri: form.avatar?.uri ?? user?.avatar}}
             style={{
               borderColor:'#FFFFFF',
               borderWidth: 4.5,
@@ -88,12 +124,12 @@ const edit = () => {
             className="w-[120px] h-[120px] rounded-full m-4"
             resizeMode='cover'
           />
-            <Image
-              source={images.changePhoto}
-              className={"w-12 h-12 absolute m-[52px]"}
-              resizeMode='contain'
-              tintColor={"#636363"}
-            />
+          <Image
+            source={images.changePhoto}
+            className={"w-12 h-12 absolute translate-x-[52px] translate-y-[52px]"}
+            resizeMode='contain'
+            tintColor={"#636363"}
+          />
         </TouchableOpacity>
       </View>
       <View className="w-full px-6">
